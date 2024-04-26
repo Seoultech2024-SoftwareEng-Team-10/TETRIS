f6a44966dfed272dfcb82198b9b26abb764b081c
성민서 <seongminseo1010@gmail.com>
2024-04-26 09:27:23 +0900

    fix: 속도 증가량, 점수획득 증가량 다시 설정;;

--- src/main/java/HelloApplication.java
+++ src/main/java/HelloApplication.java
@@ -55,7 +55,7 @@ public class HelloApplication extends Application {
     private Label scoreLabel;
     private double Frame = 1000000000;
     private static double scoreMultiplier = 1.0;
-    private static double frameMultiplier = 1.0;
+    private static double frameMultiplier = 0.8;
     private JdbcConnecter scoreboardDataInserter;
     private Text scoretext;
     private User user;
@@ -88,16 +88,17 @@ public class HelloApplication extends Application {
         Frame = 1000000000;
         running = true;
         if(LevelConstants.getLevel()=='E'){
-            Frame = 1200000000;
+            frameMultiplier = 0.8;
+            scoreMultiplier = 1.0;
         }
         else if(LevelConstants.getLevel()=='N'){
-            Frame = 1000000000;
+            frameMultiplier = 1.0;
+            scoreMultiplier = 1.2;
         }
         else{
-            Frame = 800000000;
+            frameMultiplier = 1.2;
+            scoreMultiplier = 1.4;
         }
-        System.out.println(scoreMultiplier);
-        System.out.println(frameMultiplier);
 
         waitObj = Controller.waitingTextMake(BlockColor.colorBlindMode, difficultylevel);
         nextObj = Controller.makeText(BlockColor.colorBlindMode, difficultylevel);//makeRect->makeText
