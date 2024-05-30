5b52f4ee53f41061eef680d694917c61e1ab6459
ezcho <ezcho886@naver.com>
2024-05-31 01:23:28 +0900

    feat: terminate버튼 추가

--- src/main/java/HelloApplication.java
+++ src/main/java/HelloApplication.java
@@ -50,6 +50,7 @@ public class HelloApplication extends Application {
     private static int linesNo = 0;
     private Button restartButton;
     private Button exitButton;
+    private Button terminateButton;
     private Label scoreLabel;
     private double Frame = 1000000000;
     private static double scoreMultiplier = 1.0;
@@ -172,14 +173,19 @@ public class HelloApplication extends Application {
         exitButton.setLayoutY(YMAX/2+30);
         exitButton.setVisible(false); // 초기에는 보이지 않게 설정
 
+        terminateButton = new Button("게임 나가기");
+        terminateButton.setLayoutX(XMAX / 2);
+        terminateButton.setLayoutY(YMAX / 2 + 60);
+        terminateButton.setVisible(false);
+
+
         // 버튼 이벤트 핸들러 설정
-        restartButton.setOnAction(e -> {
-            startAnimation();
-        });
+        restartButton.setOnAction(e -> {startAnimation();});
         exitButton.setOnAction(e -> GameStopped(stage));
+        terminateButton.setOnAction(e->System.exit(0));
 
         // 그룹에 버튼 추가
-        group.getChildren().addAll(restartButton, exitButton);
+        group.getChildren().addAll(restartButton, exitButton,terminateButton);
         timer = new AnimationTimer() {
             private long lastUpdate = 0;
 
